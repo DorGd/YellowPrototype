8ef3779bbee573b705663960523197f6289d7976
DorGd <37707720+DorGd@users.noreply.github.com>
2021-03-15 21:13:50 +0200

    Adding interactable UI / GameManager / EventSystem

---
+++ Assets/GameManager.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager _instance;
+    //public Inventory inventory;
+    //public Clock clock;
+    
+    //Constructor
+    public static GameManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = FindObjectOfType<GameManager>();
+                if (_instance == null)
+                {
+                    _instance = new GameObject().AddComponent<GameManager>();
+                }
+            }
+            return _instance;
+        }
+    }
+
+    private void Awake()
+    {
+        if (_instance != null)
+        {
+            Destroy(this);
+            DontDestroyOnLoad(this);
+        }
+    }
+
+
+}
--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -1,6 +1,8 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using System
+
 
 [RequireComponent(typeof(PlayerAI))]
 public class Controller : MonoBehaviour
@@ -8,14 +10,20 @@ public class Controller : MonoBehaviour
     private PlayerControls _playerControls;
     private Camera _cam;
     private PlayerAI _ai;
-    public LayerMask movementMask;
-    public LayerMask obstructionMask;
+    private LayerMask _leftMouseMask;
+    private LayerMask _rightMouseMask;
+    private Button[] _buttons;
+
+    public Canvas rightClickCanvas;
 
     private void Awake()
     {
         _playerControls = new PlayerControls();
         _ai = GetComponent<PlayerAI>();
         _cam = Camera.main;
+        _leftMouseMask = LayerMask.GetMask("Ground", "Obstruction");
+        _rightMouseMask = LayerMask.GetMask("Interactable");
+        _buttons = rightClickCanvas.GetComponentsInChildren<Button>(true);
     }
 
     private void OnEnable()
@@ -36,20 +44,25 @@ public class Controller : MonoBehaviour
 
         // Read Right Muse value
         bool interactInput = _playerControls.Player.Interact.triggered;
+
+
         if (goInput)
         {
             Vector2 mousePos = _playerControls.Player.MosuePosition.ReadValue<Vector2>();
             RaycastHit hit;
             Ray ray = _cam.ScreenPointToRay(mousePos);
-            if (Physics.Raycast(ray, out hit, 100f, obstructionMask))
+            if (Physics.Raycast(ray, out hit, 100f, _leftMouseMask) && !IsMouseOverUI())
             {
-                return;
-
-            }
-            else if (Physics.Raycast(ray, out hit, 100f, movementMask))
-            {
-                // Move player to hit point
-                _ai.MoveToPoint(hit.point);
+                rightClickCanvas.enabled = false;
+                switch (LayerMask.LayerToName(hit.transform.gameObject.layer))
+                {
+                    case "Obstruction":
+                        return;
+                    case "Ground":
+                        // Move player to hit point
+                        _ai.MoveToPoint(hit.point);
+                        break;
+                }
 
             }
         }
@@ -59,13 +72,30 @@ public class Controller : MonoBehaviour
             Vector2 mousePos = _playerControls.Player.MosuePosition.ReadValue<Vector2>();
             RaycastHit hit;
             Ray ray = _cam.ScreenPointToRay(mousePos);
-            if (Physics.Raycast(ray, out hit, 100f))
+            if (Physics.Raycast(ray, out hit, 100f, _rightMouseMask))
             {
-                // Look for IInteract interface
-                // and if so do something (open options ui / focus / interact() 
+              
+                //Interactable item = hit.transform.gameobject.GetComponenet<Interactable>();
+                //Actions[] events = item.calcInteractions();
+                //for (int i = 0; i < events.Length ; i++)
+                //{
+                //    Button btn = _buttons[i];
+                //    Text txt = btn.GetComponentInChildren<Text>();
+                //    txt.text = events[i].Method.Name
+                //    btn.onClick.RemoveAllListeners();
+                //    btn.onClick += events[i];
 
+                //}
+                rightClickCanvas.transform.position = hit.point;
+                rightClickCanvas.enabled = true;
+                
             }
         }
 
+    }
+
+    private bool IsMouseOverUI()
+    {
+        return EventSystem.current.IsPointerOverGameObject();    
     }
 }
