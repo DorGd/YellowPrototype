3405d7ed4142a8bc54f096ed9e1b750aa56c3efc
IdoLazer <idolazer8@gmail.com>
2021-05-25 19:08:52 +0300

    rollback on doorcontroller script

--- Assets/Scripts/Environment/DoorController.cs
+++ Assets/Scripts/Environment/DoorController.cs
@@ -14,11 +14,7 @@ public class DoorController : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-<<<<<<< HEAD
-        if (other.gameObject.CompareTag("npc") || other.gameObject.CompareTag("Player"))
-=======
         if (other.gameObject.CompareTag("NPC"))
->>>>>>> main
         {
             OpenDoor();
         }
@@ -26,11 +22,7 @@ public class DoorController : MonoBehaviour
 
     private void OnTriggerExit(Collider other)
     {
-<<<<<<< HEAD
-        if (other.gameObject.CompareTag("npc") || other.gameObject.CompareTag("Player"))
-=======
         if (other.gameObject.CompareTag("NPC"))
->>>>>>> main
         {
             CloseDoor();
         }
