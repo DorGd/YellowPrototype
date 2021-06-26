5e66be54ed6da0b448723968acf6f2bc5ad3c385
DorGd <37707720+DorGd@users.noreply.github.com>
2021-06-26 14:20:30 +0300

    commit before pull

---
+++ Assets/Scripts/Paradigm/WarningSanction.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Paradigm/Sanctions/Warning")]
+public class WarningSanction : SanctionSO
+{
+        
+    public override void Apply(EnemyManager enemy)
+    {
+        GameManager.Instance.SpeechManager.StartSpeech(enemy.transform.position ,enemy.CurrentParadigm.text);
+    }
+}
